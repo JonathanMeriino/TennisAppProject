244a0c5ca21e284677430d699e6060c9970dcf01
JonathanMerino <jbenitez.m14@gmail.com>
2025-12-10 18:26:57 -0600

    add admin .sites

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
-from .models import Categoria
+from .models import (
+    Categoria, Roles, Formato, Usuario, Torneo, Cuadro, GruposCategoria, 
+    Inscripciones, Disponibilidad, MiembrosGrupo, PosicionesGrupo, Partido, Sets
+)
 # Register your models here.
 
+# --- 1. Tablas Base e Independientes ---
+
+# Registro simple de las tablas sin personalización
 admin.site.register(Categoria)
+admin.site.register(Roles)
+admin.site.register(Formato)
+
+# --- 2. Tablas Principales ---
+
+admin.site.register(Usuario)
+admin.site.register(Torneo)
+admin.site.register(Cuadro)
+admin.site.register(GruposCategoria)
+
+# --- 3. Tablas de Relación y Movimiento ---
+
+admin.site.register(Inscripciones)
+admin.site.register(Disponibilidad)
+admin.site.register(MiembrosGrupo)
+admin.site.register(PosicionesGrupo)
+admin.site.register(Partido)
+admin.site.register(Sets)
