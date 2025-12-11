fb873ee7db738c165c6def526f057931b3ff5e73
JonathanMerino <jbenitez.m14@gmail.com>
2025-12-10 18:36:50 -0600

    view set categories

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import CategoriaSerializer
+from .models import Categoria
 
 # Create your views here.
+
+class CategoriaViewSet((viewsets.ModelViewSet)):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
