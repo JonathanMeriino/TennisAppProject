0216cded184597b96daa34c92bc00d853be20cc0
JonathanMerino <jbenitez.m14@gmail.com>
2025-12-10 18:44:13 -0600

    add router

---
+++ myapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework import routers
+from myapp import views
+
+router = routers.DefaultRouter
+router.register(r'categoria',views.CategoriaViewSet)
+
+urlpatterns = [
+    path('',include(router.urls))
+]
