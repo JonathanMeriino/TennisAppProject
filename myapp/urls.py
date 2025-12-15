5365f1dd16e3438a4f6b0fa5b2d203ac243f0835
JonathanMerino <jbenitez.m14@gmail.com>
2025-12-15 13:50:44 -0600

    creacion de los endpoints

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,9 +2,27 @@ from django.urls import path, include
 from rest_framework import routers
 from myapp import views
 
+#Inicializar el router
 router = routers.DefaultRouter()
-router.register(r'categoria',views.CategoriaViewSet) # r especificar la base categorias
 
+#Registrar cada view set con su URL base (endpoint)
+
+router.register(r'categoria',views.CategoriaViewSet) # r especificar la base categorias
+router.register(r'roles', views.RolesViewSet)
+router.register(r'formato', views.FormatoViewSet)
+router.register(r'usuario',views.UsuarioViewSet)
+router.register(r'torneo', views.TorneoViewSet)
+router.register(r'cuadro', views.CuadroViewSet)
+router.register(r'gruposCategoria', views.GruposCategoriaViewSet)
+router.register(r'inscripciones',views.InscripcionesViewSet)
+router.register(r'disponibilidad', views.DisponibilidadViweSet)
+router.register(r'miembrosGrupo',views.MiembrosGrupoViewSet)
+router.register(r'posicionesGRupo', views.PosicionesGrupoViewSet)
+router.register(r'partido', views.PartidoViewSet)
+router.register(r'sets', views.SetsViewSet)
+
+
+#Incluye todas las rutas generadas por el Router
 urlpatterns = [
     path('',include(router.urls))
 ]
