e9bdb92543d4f572f6e99a816c93be140e9b1122
JonathanMerino <jbenitez.m14@gmail.com>
2025-12-15 13:23:36 -0600

    crear viewsets para los serializers

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,62 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializer import CategoriaSerializer
-from .models import Categoria
+from .serializer import (CategoriaSerializer, RolesSerializer, FormatoSerializer, UsuarioSerializer, TorneoSerializer, CuadroSerializer, GruposCategoriaSerializer, InscripcionesSerializer, DisponibilidadSerializer, MiembrosGrupoSerializaer, PosicionesGrupoSerializer, PartidoSerializer , SetsSerializer)
+from .models import (
+    Categoria, Roles, Formato, Usuario, Torneo, Cuadro, GruposCategoria, 
+    Inscripciones, Disponibilidad, MiembrosGrupo, PosicionesGrupo, Partido, Sets
+)
 
 # Create your views here.
 
 class CategoriaViewSet(viewsets.ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+
+class RolesViewSet(viewsets.ModelViewSet):
+    queryset = Roles.objects.all()
+    serializer_class = RolesSerializer
+    
+class FormatoViewSet(viewsets.ModelViewSet):
+    queryset = Formato.objects.all()
+    serializer_class = FormatoSerializer
+
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+class TorneoViewSet(viewsets.ModelViewSet):
+    queryset = Torneo.objects.all()
+    serializer_class = TorneoSerializer
+
+class CuadroViewSet(viewsets.ModelViewSet):
+    queryset = Cuadro.objects.all()
+    serializer_class = CuadroSerializer
+
+class GruposCategoriaViewSet(viewsets.ModelViewSet):
+    queryset = GruposCategoria.objects.all()
+    serializer_class = GruposCategoriaSerializer
+
+class InscripcionesViewSet(viewsets.ModelViewSet):
+    queryset = Inscripciones.objects.all()
+    serializer_class = InscripcionesSerializer
+
+class DisponibilidadViweSet(viewsets.ModelViewSet):
+    queryset = Disponibilidad.objects.all()
+    serializer_class = DisponibilidadSerializer
+
+
+class MiembrosGrupoViewSet(viewsets.ModelViewSet):
+    queryset = MiembrosGrupo.objects.all()
+    serializer_class = MiembrosGrupoSerializaer
+
+class PosicionesGrupoViewSet(viewsets.ModelViewSet):
+    queryset = PosicionesGrupo.objects.all()
+    serializer_class = PosicionesGrupoSerializer
+
+class PartidoViewSet(viewsets.ModelViewSet):
+    queryset = Partido.objects.all()
+    serializer_class = PartidoSerializer
+
+class SetsViewSet (viewsets.ModelViewSet):
+    queryset = Sets.objects.all()
+    serializer_class = SetsSerializer
