b9f28b570a0775ace3d52da935d1a52804fb2978
JonathanMerino <jbenitez.m14@gmail.com>
2025-12-16 14:18:31 -0600

    actualizar base de datos a postgresql

--- tenis_app/settings.py
+++ tenis_app/settings.py
@@ -76,13 +76,24 @@ WSGI_APPLICATION = "tenis_app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
+#SQLITE
+""" DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
+} """
+#POSTGRESQL
+DATABASES = {
+    "default": {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tennisapp',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
