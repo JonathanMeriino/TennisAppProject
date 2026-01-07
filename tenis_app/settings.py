ff5456c0466f9a63563f143a059fd71fa7204d67
JonathanMerino <jbenitez.m14@gmail.com>
2026-01-06 18:33:38 -0600

    Actualizar "settings" para registrar JWT como el metodo de autenticacion oficial

--- tenis_app/settings.py
+++ tenis_app/settings.py
@@ -140,6 +140,12 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
+    'DEFAULT_AUTHENTICATION_CLASSES': ( 
+        'rest_framework_simplejwt.authentication.JWTAuthentication', 
+        ), 
+    'DEFAULT_PERMISSION_CLASSES': ( 
+        'rest_framework.permissions.IsAuthenticated', 
+        ),
 }
 
 CORS_ALLOWED_ORIGINS = [
