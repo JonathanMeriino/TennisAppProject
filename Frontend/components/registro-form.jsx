30d390bb5dbace992642827d3ade3c0858b07ec1
JonathanMerino <jbenitez.m14@gmail.com>
2026-09-24 12:47:26 -0600

    EScuela de procedencia

--- Frontend/components/registro-form.jsx
+++ Frontend/components/registro-form.jsx
@@ -238,7 +238,7 @@ export function RegistroForm() {
             />
           </div>
           <div className="space-y-2">
-            <label className="text-xs font-semibold text-foreground block">Escuela / Institución</label>
+            <label className="text-xs font-semibold text-foreground block">Escuela de procedencia</label>
             <input
               type="text"
               name="escuela"
